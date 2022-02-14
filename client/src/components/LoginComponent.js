2edced3a630a66ec2e51857c9541671610831d27
Minyahel Fekadu Haile <minyahelfekadu13@gmail.com>
2022-02-14 16:02:29 +0100

    No change

--- client/src/components/LoginComponent.js
+++ client/src/components/LoginComponent.js
@@ -5,14 +5,15 @@ class Login extends Component {
         super(props);
         this.state = {
             email: null,
-            password: null
+            password: null,
+            response: null
         };
         this.handleLogin = this.handleLogin.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
     }
 
-    handleLogin(event) {
+    handleLogin(e) {
         fetch('/user/login', {
             method: 'POST',
             headers: {
@@ -24,6 +25,8 @@ class Login extends Component {
             })
         })
             .then((res) => {
+                //make sure you return functions that returns promises
+                //to chain .then statements
                 return res.json();
             })
             .then((res) => {
