1bc9eb92cf171653e5f2d75171aa1373d242314c
Minyahel Fekadu Haile <minyahelfekadu13@gmail.com>
2022-03-02 09:35:48 +0100

    Delete config.js

--- util/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoUrl:
-        'mongodb+srv://mhaile:F4AvsRdoUXcjz3ZB@cluster0.mp73v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    mongoStoreUrl:
-        'mongodb+srv://mhaile:F4AvsRdoUXcjz3ZB@cluster0.mp73v.mongodb.net/sessionStore?retryWrites=true&w=majority'
-};
